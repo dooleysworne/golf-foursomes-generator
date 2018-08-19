fbbfaa6e5eae81e63182a8928ae88626283fd5d5
dooleysworne <35536000+dooleysworne@users.noreply.github.com>
2018-08-19 09:47:35 -0500

    Add files via upload

--- foursomes.php
+++ foursomes.php
@@ -1,11 +1,73 @@
-<?php //include("userauth.php"); ?>
+<?php //include("userauth.php");
+session_start();
+$SESSION["radios"] = $_REQUEST["radios"];
+$d=$SESSION["radios"];
+?>
 <html>
 <head>
-<title>Disc Golf Player Foursomes/Threesomes Randomizer</title>
+<title>Disc Golf Player Foursomes/Threesomes/Twosomes Randomizer</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <style>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {display:none;}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+ /* background-color: #2196F3; */
+  background-color: #f22121;
+}
+
+input:focus + .slider {
+ /* box-shadow: 0 0 1px #2196F3; */
+  box-shadow: 0 0 1px #f22121;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
 .input_fields_wrap{
 max-width: 350px;
 }
@@ -22,16 +84,40 @@ H4 {
 </style>
 </head>
 <body>
+
 <div class="w3-responsive w3-margin">
-[ Go To <a href="doubles.php" target="_blank">Doubles Randomizer</a> ]  [ Go To <a href="threesomes.php" target="_blank">4somes/3somes -or- 3somes/2somes</a> ]
-<H4>FOURSOMES/THREESOMES RANDOM GENERATOR</H4>
+[ Go To <a href="doubles.php" target="_blank">Doubles Randomizer</a> ]
+<H4>FOURSOMES/THREESOMES/TWOSOMES RANDOM GENERATOR</H4>
 <hr>
+<form name="form" method="REQUEST" action="foursomes.php">
+<table>
+<tr>
+<td>
+<?php if($_REQUEST["max"]=='3' || $_REQUEST["radios"]=='3') {
+echo '<label class="switch"><input name="radios" id="switch" type="checkbox" value="3" checked / onchange="this.form.submit()"><span class="slider round"></span></label> Threesomes/Twosomes';
+} else {
+echo '<label class="switch"><input name="radios" id="switch" type="checkbox" value="3" / onchange="this.form.submit()"><span class="slider round"></span></label> Threesomes/Twosomes';
+}
+?>
+</td></tr>
+</table>
+</form>
+<?php
+//if($d != '') {
+if($_REQUEST["max"]=='3' || $_REQUEST["radios"]=='3') {
+echo "<fieldset>Your groups will have a maximimum number of <b>3 players</b> in them</fieldset>";
+} else {
+echo "<fieldset>Your groups will have a maximimum number of <b>4 players</b> in them</fieldset>";
+$d='4';
+}
+?>
 <?php
 if(!$_REQUEST["checkbox"]) {
 echo "<b>Use The Checkboxes Below To Select Players For Groups To Play Today</b>";
 echo "<fieldset>";
 ?>
-<form action="foursomes.php" method="REQUEST" id="panelone">
+<form name="form2" action="foursomes.php" method="REQUEST" id="panelone">
+<input name="max" type="hidden" value="<?php echo $d; ?>">
 
 <table border="0" class="w3-table-all">
 <tr class="w3-red">
@@ -53,7 +139,7 @@ foreach ($row as $key => $value) {
 ?>
 <tr>
 <td align="center" bgcolor="#FFFFFF">
-<input class="w3-check" name="checkbox[]" type="checkbox" value="<?php echo $csv[$key][0];?>" />
+<input class="w3-check" name="checkbox[]" type="checkbox" class="first" value="<?php echo $csv[$key][0];?>" />
 </td>
 <td align="center">
 <input name="user[]" type="hidden" id="user" value="<?php echo $csv[$key][0];?>"><?php echo $csv[$key][0]; ?>
@@ -85,6 +171,10 @@ echo "&nbsp;&nbsp;";
 echo "<button class='w3-button w3-red' onclick=\"myFunction()\">Mix This List Again</button>";
 echo "<br><br>\n";
 echo "</div>";
+if(!$_REQUEST["radios"] && $_REQUEST["max"]=='') {
+echo "<fieldset><b>Oops! Please Click the Reset Button and Select Threesomes/Twosomes or Foursomes/Threesomes First</fieldset>";
+die();
+}
 echo "<fieldset class='w3-responsive w3-margin'>";
 echo "<legend><b>RESULTS</b></legend>\n";
 
@@ -110,8 +200,11 @@ $howmany = count($players);
 
 echo "<i>There are ".$howmany." golfers arranged in ";
 
+if(!$_SESSION['$radios'] || $d = '') {
+$d = $_REQUEST['max'];
+}
 if($howmany<'6') { $numgroups = '1'; } else {
-$numgroups = ceil($howmany/4);
+$numgroups = ceil($howmany/$d);
 }
 echo $numgroups." groups</i><br/><br/>";
 
@@ -148,9 +241,8 @@ echo "</div>";
 function myFunction() {
     location.reload();
 }
-</script>
 
-<script>
+
 var max_fields      = 16;
 var wrapper         = $(".input_fields_wrap");
 var add_button      = $(".add_field_button");
@@ -171,11 +263,12 @@ $(remove_button).click(function(e){
 	}
 });
 
+
 $('.selectall').click(function() {
     if ($(this).is(':checked')) {
-        $('input:checkbox').attr('checked', true);
+     $('input[type="checkbox"]').not('#switch').attr('checked', true);
     } else {
-        $('input:checkbox').attr('checked', false);
+     $('input[type="checkbox"]').not('#switch').attr('checked', false);
     }
 });
 </script>
